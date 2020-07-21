69eedc12ab4bc0b86fe8a54b1c7f5bb2eb986805
100117 <vahdet.keskin@isbank.com.tr>
2020-07-21 21:31:34 +0300

    add resource - kinesis data stream

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region  = "us-west-1" # Ireland
+}
+
+resource "aws_kinesis_stream" "general_comment_data_stream" {
+  name                = "comment-data-stream"
+  shard_count         = 1
+  retention_period    = 24
+  encryption_type     = "NONE"
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+  tags = {
+    application = "hush-off"
+    environment = var.env
+  }
 }
